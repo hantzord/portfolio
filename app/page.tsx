96dc179cb7372f0edb40eb66133b660fb85fec72
Alfian Nur Fazar Rizky <145629765+hantzord@users.noreply.github.com>
2026-04-20 23:33:15 +0700

    adding certificate button link in about section

--- app/page.tsx
+++ app/page.tsx
@@ -548,6 +548,16 @@ export default function Home() {
                       )
                     },
                     {
+                      label: 'Certificates',
+                      href: 'https://drive.google.com/drive/folders/1C40QHwahuDb617YKzo7H7-b9e7pVjXJo?usp=sharing',
+                      rotate: '1.5deg',
+                      icon: (
+                        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                        </svg>
+                      )
+                    },
+                    {
                       label: 'Email',
                       href: 'mailto:alfiannfr@gmail.com',
                       rotate: '2.5deg',
